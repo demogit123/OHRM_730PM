96ead453ee13d2eeda31358e8aa3bcfab8d3f86c
Krish <Krish@MSI.alphionsee.in>
2023-06-26 20:52:40 +0530

    this is testcase 3

---
+++ MyProject/src/testcases/testcase3.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class testcase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is testcase 3");
+
+	}
+
+}
